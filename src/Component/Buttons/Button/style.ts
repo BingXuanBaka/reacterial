75d1dc2276d14628bd0a6a23c4c97904a9787cb6
BingXuanOwO <hxc35459@outlook.com>
2023-05-08 20:51:34 +0800

    refactor(button componet): edit  common button

--- src/Component/Buttons/Button/style.ts
+++ src/Component/Buttons/Button/style.ts
@@ -39,7 +39,15 @@ export default (theme: Theme, dark: boolean) => {
                 top: 0,
                 transition: '.2s',
                 opacity: 0
-            }
+            },
+
+            '&:hover::after': {
+                opacity: 0.08
+            },
+
+            '&:active::after': {
+                opacity: 0.12
+            },
         }),
 
         filled: css({
@@ -48,20 +56,16 @@ export default (theme: Theme, dark: boolean) => {
             color: hexFromArgb(scheme.onPrimary),
             boxSizing: 'border-box',
 
+
+            '&::after': {
+                backgroundColor: hexFromArgb(scheme.onPrimary),
+            },
+
             '&:hover': {
                 ...elevation.level1
             },
-            '&:hover::after': {
-                backgroundColor: hexFromArgb(scheme.onPrimary),
-                opacity: 0.08
-            },
-
             '&:active': {
                 boxShadow: ""
-            },
-            '&:active::after': {
-                backgroundColor: hexFromArgb(scheme.onPrimary),
-                opacity: 0.12
             },
         }),
 
@@ -69,36 +73,26 @@ export default (theme: Theme, dark: boolean) => {
             backgroundColor: hexFromArgb(scheme.secondaryContainer),
             color: hexFromArgb(scheme.onSecondaryContainer),
 
+            '&::after': {
+                backgroundColor: hexFromArgb(scheme.onSecondaryContainer),
+            },
+
             '&:hover': {
                 ...elevation.level1
             },
-            '&:hover::after': {
-                backgroundColor: hexFromArgb(scheme.onSecondaryContainer),
-                opacity: 0.08
-            },
-
             '&:active': {
                 boxShadow: ""
             },
-            '&:active::after': {
-                backgroundColor: hexFromArgb(scheme.onSecondaryContainer),
-                opacity: 0.12
-            },
+
         }),
 
         elevated: css({
             color: hexFromArgb(scheme.primary),
-            backgroundColor: hexFromArgb(getSurfaceColorFromScheme(scheme).surface1),
+            backgroundColor: hexFromArgb(getSurfaceColorFromScheme(scheme).surface3),
             ...elevation.level1,
 
-            '&:hover::after': {
+            '&::after': {
                 backgroundColor: hexFromArgb(scheme.primary),
-                opacity: 0.08
-            },
-
-            '&:active::after': {
-                backgroundColor: hexFromArgb(scheme.primary),
-                opacity: 0.12
             },
         }),
 
@@ -110,14 +104,8 @@ export default (theme: Theme, dark: boolean) => {
                 `,
             color: hexFromArgb(scheme.primary),
             
-            '&:hover::after': {
+            '&::after': {
                 backgroundColor: hexFromArgb(scheme.onSurface),
-                opacity: 0.08
-            },
-
-            '&:active::after': {
-                backgroundColor: hexFromArgb(scheme.onSurface),
-                opacity: 0.12
             },
         }),
 
@@ -126,14 +114,8 @@ export default (theme: Theme, dark: boolean) => {
             padding: '0 12px',
             color: hexFromArgb(scheme.primary),
                         
-            '&:hover::after': {
+            '&::after': {
                 backgroundColor: hexFromArgb(scheme.onSurface),
-                opacity: 0.08
-            },
-
-            '&:active::after': {
-                backgroundColor: hexFromArgb(scheme.onSurface),
-                opacity: 0.12
             },
         })
 
