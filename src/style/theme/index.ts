8ed03442802b920642b778a8e0026af74dc8cc94
BingXuanOwO <hxc35459@outlook.com>
2023-07-01 16:23:24 +0800

    feat: replace getScheme function

--- src/style/theme/index.ts
+++ src/style/theme/index.ts
@@ -1,21 +1,24 @@
-import { Blend, CorePalette, Hct, Scheme, Theme, argbFromHex, themeFromSourceColor } from "@material/material-color-utilities"
+import { Blend, Scheme, Theme, argbFromHex, themeFromSourceColor } from "@material/material-color-utilities"
 import { createContext, useContext } from "react"
 
 type ColorScheme = 'system' | 'light' | 'dark'
 
-
 export const ThemeContext = createContext<{
-    theme: Theme, 
+    theme: Theme,
     colorScheme?: ColorScheme
 }>({
     theme: themeFromSourceColor(argbFromHex("6751a5"))
 })
 
-export const ColorSchemeContext = createContext<ColorScheme>('system')
+export function getTheme(): Theme {
+    const theme = useContext(ThemeContext).theme
+    return theme
+}
 
-export function getScheme(): ColorScheme {
-    const scheme = useContext(ColorSchemeContext)
-    return scheme
+export function getScheme(): Scheme {
+    return getIsDark() ?
+        getTheme().schemes.dark :
+        getTheme().schemes.light
 }
 
 export function getIsDark(): boolean {
@@ -24,11 +27,6 @@ export function getIsDark(): boolean {
         return window.matchMedia && window.matchMedia(' (prefers-color-scheme: dark)').matches
     }
     return scheme == 'dark'
-}
-
-export function getTheme(): Theme {
-    const theme = useContext(ThemeContext).theme
-    return theme
 }
 
 export function getSurfaceColorFromScheme(
@@ -40,7 +38,6 @@ export function getSurfaceColorFromScheme(
     surface4: number,
     surface5: number,
 } {
-
     return {
         surface1: Blend.cam16Ucs(scheme.surface, scheme.primary, 0.05),
         surface2: Blend.cam16Ucs(scheme.surface, scheme.primary, 0.08),
@@ -51,11 +48,8 @@ export function getSurfaceColorFromScheme(
     }
 }
 
-export function getSurfaceElevationColor(scheme: Scheme, elevation: 0 | 1 | 2 | 3 | 4 | 5) {
+export function getSurfaceElevationColor(scheme: Scheme, elevation: 0 | 1 | 2 | 3 | 4 | 5): number{
     switch (elevation) {
-        case 0:
-            return scheme.surface
-
         case 1:
             return Blend.cam16Ucs(scheme.surface, scheme.primary, 0.05)
 
@@ -70,6 +64,8 @@ export function getSurfaceElevationColor(scheme: Scheme, elevation: 0 | 1 | 2 | 
 
         case 5:
             return Blend.cam16Ucs(scheme.surface, scheme.primary, 0.14)
-    }
+    } 
+
+    return scheme.surface
 }
 
